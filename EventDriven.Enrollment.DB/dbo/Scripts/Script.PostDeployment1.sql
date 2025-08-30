e577eedca55fd4ebdea0b237041e05ed2c499bcd
Jerome Rosales <131425913+Jaydee1430@users.noreply.github.com>
2025-08-30 19:44:59 +0800

    Update Script.PostDeployment1.sql

--- EventDriven.Enrollment.DB/dbo/Scripts/Script.PostDeployment1.sql
+++ EventDriven.Enrollment.DB/dbo/Scripts/Script.PostDeployment1.sql
@@ -3,4 +3,4 @@ INSERT INTO Users (Username, Password, Role)
 VALUES 
 ('admin', 'password', 'Admin'),
 ('registrar', 'password', 'Registrar'),
-('cashier', 'password', 'Cashier_user');
+('cashier', 'password', 'Cashier');
